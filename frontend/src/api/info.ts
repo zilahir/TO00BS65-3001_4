208cb9264219a722ff02236784db1131c80fe340
Richard Zilahi <richard.zilahi@varjo.com>
2022-05-09 14:05:59 +0300

    implemented info

--- frontend/src/api/info.ts
+++ frontend/src/api/info.ts
@@ -8,6 +8,12 @@ interface BasicInfo {
     [key: string]: string
 }
 
+interface PreviousAssignments {
+    key: number,
+    name: string,
+    githubUrl: string,
+}
+
 interface Info {
     basicInfo: BasicInfo,
     description: () => Description[],
@@ -15,6 +21,8 @@ interface Info {
     getAllDescription: () => Description[],
     i18n: BasicInfo
     getAlli18nKey: () => BasicInfo,
+    previousAssignments: PreviousAssignments[],
+    getAllPreviousAssignments: () => PreviousAssignments[]
 }
 
 const info: Info = {
@@ -32,6 +40,13 @@ const info: Info = {
         assignMentName: 'REACT front-end',
         githubRepoUrl: 'https://github.com/zilahir/TO00BS65-3001_4' 
     },
+    previousAssignments: [
+        { key: 1, name: 'Guestbook', githubUrl: 'https://github.com/zilahir/TO00BS65-3001'},
+        { key: 2, name: 'Guestbook with template engine', githubUrl: 'https://github.com/zilahir/TO00BS65-3001_2'},
+        { key: 3, name: 'Rest API', githubUrl: 'https://github.com/zilahir/TO00BS65-3001_3'},
+        { key: 3, name: 'REACT front-end', githubUrl: 'https://github.com/zilahir/TO00BS65-3001_4'},
+
+    ],
     description: () => [
         { key: 1, text: `This application was written for the course ${info.basicInfo.courseCode}, at Laurea.`},
         { key: 2, text: 'The application\'s README file can be found on the GitHub repository' },
@@ -39,6 +54,7 @@ const info: Info = {
     getAlli18nKey: () => info.i18n,
     getBasicInfo: () => info.basicInfo,
     getAllDescription: () => info.description(),
+    getAllPreviousAssignments: () => info.previousAssignments
 }
 
 export default info
--- frontend/src/components/Info/index.tsx
+++ frontend/src/components/Info/index.tsx
@@ -6,14 +6,15 @@ import InfoApi from '../../api/info';
 function Info(): ReactElement {
     const basicInfo = InfoApi.getBasicInfo()
     const i18nKeys = InfoApi.getAlli18nKey()
+    const previousAssignments = InfoApi.getAllPreviousAssignments()
     return (
         <div className={styles.infoContainer}>
             <div className={styles.basicInfo}>
                 {
                     Object.keys(basicInfo).map((key) => (
-                        <p className={styles.meta}>
+                        <p key={basicInfo.key} className={styles.meta}>
                             <span>
-                                {i18nKeys[key]}
+                                {i18nKeys[key]}:
                             </span>
                             <span>
                             {basicInfo[key]}
@@ -21,6 +22,22 @@ function Info(): ReactElement {
                         </p>
                     ))
                 }
+            </div>
+            <div className={styles.basicInfo}>
+                <div className={styles.prevAssignMents}>
+                    {
+                        previousAssignments.map(({ key, name, githubUrl }, index) => (
+                            <div key={key} className={styles.singlePreviousAssignment}>
+                                <p>
+                                    {index+1}: {name}
+                                </p>
+                                <p>
+                                    <a rel="noopener noreferrer" target="_blank" href={githubUrl}>[github url]</a>
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
